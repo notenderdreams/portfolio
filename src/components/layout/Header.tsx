5bc78935655fb5ad665ae42e389878153bd28b28
Nahian <enderdreams@outlook.com>
2026-08-26 23:09:56 +0600

    style: spread out topbar navigation and lighten links

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { siteMetadata, navLinks } from '../../data/metadata';
+import { navLinks } from '../../data/metadata';
 
 export const Header: React.FC = () => {
   return (
     <header className="site-header">
-      <a className="brand-mark" href="#top">
-        {siteMetadata.brandMark}
-      </a>
       <nav className="site-nav" aria-label="Main Navigation">
         {navLinks.map((link) => (
           <a key={link.href} href={link.href}>
@@ -17,3 +14,4 @@ export const Header: React.FC = () => {
     </header>
   );
 };
+
